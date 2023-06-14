e9682e86102ee19a3820231d08bf4bb48ffe8596
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-14 22:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function changeStatus() {
+        // Get the reference to the status paragraph
+        var statusParagraph = document.getElementById("status");
+
+        // Change the text and wrap it in an h1 tag
+        statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+      }
